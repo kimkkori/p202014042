183c0aa08986b44c92f49f4e8509c584769f0733
Kim, Taeil <gkti100@gmail.com>
2024-11-20 10:01:33 +0900

    three

--- src/p202014042/Main.java
+++ src/p202014042/Main.java
@@ -2,7 +2,13 @@ package p202014042;
 
 public class Main {
 	  public static void main(String[] args) {
-	    //  자신의 학번과 이름을 출력하라
+	    // 자신의 학번과 이름을 입력하라
 	    System.out.println("202014042 김태일");
+	    System.out.println(Hello.sum(10));
+	    System.out.println(Hello.sum(100));
+	    System.out.println(Hello.sum(1000));
+	    System.out.println(World.factorial(3));
+	    System.out.println(World.factorial(6));
+	    System.out.println(World.factorial(9));
 	  }
 	}
